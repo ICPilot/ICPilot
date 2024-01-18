300b1a9725ac8c27ca3dedcdb5501aafb5fef020
Daniel Vaughan <1843788+DVaughan@users.noreply.github.com>
2024-01-18 15:05:22 +0100

    Add ICPilot solution.

---
+++ Source/ICPilot/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ICPilot
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
